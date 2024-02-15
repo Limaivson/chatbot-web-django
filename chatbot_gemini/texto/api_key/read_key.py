60a5873c7026051ba23c0871b800399aa0c25620
Limaivson <ils3@cin.ufpe.br>
2024-02-15 10:05:41 -0300

    refactor-arq-duplicate

--- chatbot_gemini/texto/api_key/read_key.py
+++
@@ -1,7 +0,0 @@
-import json
-
-
-def get_api_key():
-    with open("/Users/Ivson/Documents/GitHub/chatbot/api_key/key.json") as api_key:
-        key = json.load(api_key)
-    return str(key['key'])
