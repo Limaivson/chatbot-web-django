53ee199594216f572ebe9d898916434d9bc42c5c
Ivson De Lima Santos <124381236+Limaivson@users.noreply.github.com>
2024-02-24 19:04:44 -0300

    feat-functions-views

--- chatbot_gemini/chat/views.py
+++ chatbot_gemini/chat/views.py
@@ -1,3 +1,22 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from .utils.text.response_text import ResponseText
+import json
 
-# Create your views here.
+gemini = ResponseText()
+
+
+def index(request):
+    return render(request, 'index.html', {})
+
+
+def message(request):
+    if request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        user_message = data.get('mensagem', '')
+
+        server_response = gemini.responseGeminai(user_message)
+
+        return JsonResponse({'respostaServidor': server_response})
+
+    return JsonResponse({'error': 'Método não permitido'}, status=405)
