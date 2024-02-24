b6ed0ba9e3d0214feb6ca8b409db2d3bf5668644
Ivson De Lima Santos <124381236+Limaivson@users.noreply.github.com>
2024-02-24 19:07:29 -0300

    feat-response-image-class

---
+++ chatbot_gemini/chat/utils/image/response_image.py
@@ -0,0 +1,19 @@
+import google.generativeai as genai
+from PIL import Image
+from io import BytesIO
+from decouple import config
+
+
+class ResponseImage:
+    def __init__(self):
+        self.api_key = config('API_KEY')
+        self.genai = genai.configure(api_key=f'{self.api_key}')
+        self.model = genai.GenerativeModel('gemini-pro-vision')
+
+    def read_image(self, upload_image, message):
+        image_data = upload_image.read()
+        image = Image.open(BytesIO(image_data))
+        response = self.model.generate_content([message, image])
+        response.resolve()
+        return response.text
+    
